ca80735a23cdc20827266a3b6115a3b77983ab4b
saintkim12 <saintkim1232@gmail.com>
2021-07-21 17:15:32 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+/* eslint-env node */
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    browser: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  plugins: [
+    'svelte3',
+    'eslint-plugin-html'
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  rules: {
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  },
+  settings: {
+    // ...
+  }
+}
---
+++ snowpack.config.mjs
@@ -0,0 +1,38 @@
+/* eslint-env node */
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+/** @type {import("snowpack").SnowpackUserConfig } */
+// import proxy from 'http2-proxy'
+export default {
+  mount: {
+    /* ... */
+    public: '/',
+    src: '/dist',
+    static: {
+      url: '/',
+      static: true
+    }
+  },
+  alias: {
+    '@src': './src'
+  },
+  plugins: [
+    /* ... */
+    ['@snowpack/plugin-sass', {}],
+    '@snowpack/plugin-svelte',
+  ],
+  routes: [
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+}
